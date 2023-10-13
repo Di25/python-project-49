16e8f795cb6eb8dd1319a3f239d4eca8d929645e
Di25 <diana--dream@mail.ru>
2023-10-13 12:47:07 +0300

    b

---
+++ brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from brain_games_1.logic import logic_game
+from brain_games_1.game import game_prime
+
+
+def main():
+    logic_game(game_prime)
+
+
+if __name__ == '__main__':
+    main()
